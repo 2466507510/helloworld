42d7000b59f7c05524a9fac53637998d96567b23
lijjai <lijjai@dcits.com>
2018-08-26 09:51:39 +0800

    test

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Helloworld {
 	
 	public static void main(String[] args) {
-		System.out.println("hello world 随便改一次");
+		System.out.println("hello world 随便改一次  hhh");
 	}
 }
